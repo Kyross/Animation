03fd722acd3e2bd97263ba1edd3794978c4c5671
Gatien <gatien.gaumerais@gmail.com>
2019-05-07 11:04:58 +0200

    Added hermitespline interpolation

---
+++ TP_siaa_etudiants/src/SceneGraph/HermiteSpline.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Math/Vectorf.h>
+
+class HermiteSpline
+{
+
+private :
+	Math::Vector3f m_p0;
+	Math::Vector3f m_p1;
+	Math::Vector3f m_m0;
+	Math::Vector3f m_m1;
+
+public:
+	HermiteSpline(Math::Vector3f p0, Math::Vector3f p1, Math::Vector3f m0, Math::Vector3f m1);
+	~HermiteSpline();
+	Math::Vector3f compute(float u);
+};
+
