4141f8465009e1b3d2af0615265be8b9c35cc5c0
Gatien <gatien.gaumerais@gmail.com>
2019-06-02 22:50:58 +0200

    Finished TP3

--- TP_siaa_etudiants/src/Application/TP3_siaa.h
+++ TP_siaa_etudiants/src/Application/TP3_siaa.h
@@ -8,29 +8,80 @@
 #include <SceneGraph/Group.h>
 #include <SceneGraph/MeshVBO_v2.h>
 #include <GL/compatibility.h>
-#include <SceneGraph/Sphere.h>
-#include <HelperGl/Material.h>
-#include <HelperGl/Color.h>
-#include <SceneGraph/CoordinateSystem.h>
-#include <SceneGraph/Translate.h>
-#include <SceneGraph/BeeModel.h>
-#include <SceneGraph\HermiteSpline.h>
-#include <SceneGraph\Trajectoire.h>
+#include <HelperGL\Material.h>
+#include <HelperGL\Color.h>
+#include <Animation\SpringMassSystem.h>
+#include <SceneGraph\Patch.h>
 
 
 namespace Application
 {
+	std::pair<Math::Vector3f, Math::Vector3f> computeEuler(const Animation::SpringMassSystem::Mass & previous, const Animation::SpringMassSystem::Mass & current, float dt) {
+		Math::Vector3f newSpeed = current.m_forces * (dt / current.m_mass) + current.m_speed;
+		Math::Vector3f newPosition = current.m_position + ((current.m_speed + newSpeed) / 2.0) * dt;
+
+		return std::pair<Math::Vector3f, Math::Vector3f>(newPosition, newSpeed);
+	}
+
+	Math::Vector3f computeForce(const Animation::SpringMassSystem::Mass & mass)
+	{
+		return Math::makeVector(0.0, 0.0, -mass.m_mass*9.81);
+	}
+
+	Math::Vector3f computeLink(const Animation::SpringMassSystem::Mass & mass1, const Animation::SpringMassSystem::Mass & mass2, const Animation::SpringMassSystem::Link & link) {
+		float stiffness = 10.0f;
+		float viscosity = 0.01f;
+		Math::Vector3f damping = mass1.m_speed*viscosity;
+
+		return (mass2.m_position - mass1.m_position) * (1 - (link.m_initialLength / (mass1.m_position - mass2.m_position).norm())) * stiffness - damping;
+	}
+
+	std::pair<Math::Vector3f, Math::Vector3f> computeGround(const Animation::SpringMassSystem::Mass &previous, const Animation::SpringMassSystem::Mass &current) {
+		if (current.m_position[2] < -1.49f)
+		{
+			Math::Vector3f newPosition = Math::makeVector(current.m_position[0], current.m_position[1], -1.49f);
+			Math::Vector3f newSpeed = Math::makeVector(0.0f, 0.0f, 0.0f);
+			return ::std::pair<Math::Vector3f, Math::Vector3f>(newPosition, newSpeed);	
+		}
+		else {
+			return std::pair<Math::Vector3f, Math::Vector3f>(current.m_position, current.m_speed);
+		}
+	}
+
+	std::pair<Math::Vector3f, Math::Vector3f> computeSphere(const Animation::SpringMassSystem::Mass &previous, const Animation::SpringMassSystem::Mass &current) {
+		
+		Math::Vector3f spherePosition = Math::makeVector(0.0f, -0.3f, -1.0f);
+		
+		Math::Vector3f dist = current.m_position - spherePosition;
+
+		if (dist.norm() <= 0.22f) {
+			Math::Vector3f newPosition = previous.m_position;
+			Math::Vector3f newSpeed = Math::makeVector(0.0f, 0.0f, 0.0f);
+			return std::pair<Math::Vector3f, Math::Vector3f>(newPosition, newSpeed);
+		}
+		else {
+			return std::pair<Math::Vector3f, Math::Vector3f>(current.m_position, current.m_speed);
+		}
+
+	}
+
+	
 	class TP3_siaa : public BaseWithKeyboard
 	{
 	protected:
 		HelperGl::Camera m_camera;
 
 		SceneGraph::Group m_root;
-		HermiteSpline * m_interpolation;
-		Trajectoire * m_trajectoire;
-		int m_nbPointsTrajectoire;
 
-		float m_compteur;
+		Animation::SpringMassSystem * m_system;
+		Animation::SpringMassSystem::PatchDescriptor m_patch;
+		SceneGraph::Patch * m_patchGraph;
+
+		std::vector<int> constrainedMasses;
+
+		Animation::SpringMassSystem * m_systemGround;
+		Animation::SpringMassSystem::PatchDescriptor m_groundPatch;
+		SceneGraph::Patch * m_groundGraph;
 
 		virtual void handleKeys()
 		{
@@ -64,6 +115,18 @@ namespace Application
 				m_camera.rotateLocal(Math::makeVector(0.0f, -1.0f, 0.0f), (float)cameraSpeed*(float)getDt()*0.2);
 				m_camera.translateLocal(Math::makeVector(-cameraSpeed * (float)getDt(), 0.0f, 0.0f));
 			}
+
+			//Contraintes
+			//bool pressed = false;
+			if (m_keyboard.isPressed('r') && !constrainedMasses.empty()) {
+				//pressed = true;
+				m_keyboard.release('r');
+				int constrainedMass = constrainedMasses.back();
+				m_system->unconstrainPosition(constrainedMass);
+				//std::cout << "unconstrained id : " << constrainedMasses.back() << std::endl;
+				constrainedMasses.pop_back();
+			}
+
 		}
 
 	public:
@@ -76,6 +139,72 @@ namespace Application
 			SceneGraph::CoordinateSystem * coord = new SceneGraph::CoordinateSystem(1);
 			m_root.addSon(coord);
 
+			//Simulateur
+			m_system = new Animation::SpringMassSystem();
+			m_system->setIntegrator(computeEuler);
+			m_system->addForceFunction(computeForce);
+			m_system->addLinkForceFunction(computeLink);
+
+			{
+				//Patch
+				Math::Vector4f row1 = Math::makeVector(1.0f, 0.0f, 0.0f, -3.0f / 2.0f);
+				Math::Vector4f row2 = Math::makeVector(0.0f, 1.0f, 0.0f, -3.0f / 2.0f);
+				Math::Vector4f row3 = Math::makeVector(0.0f, 0.0f, 1.0f, 0.0f);
+				Math::Vector4f row4 = Math::makeVector(0.0f, 0.0f, 0.0f, 1.0f);
+				Math::Matrix4x4f positionMatrix(row1, row2, row3, row4);
+				m_patch = m_system->createPatch(3.0f, 50, 3.0f, 50, 1, 10.0f, positionMatrix);
+			}
+
+			//Contraintes
+			m_system->constrainPosition(m_patch.massIndex(0, 0));
+			constrainedMasses.push_back(m_patch.massIndex(0, 0));
+			m_system->constrainPosition(m_patch.massIndex(0, 50));
+			constrainedMasses.push_back(m_patch.massIndex(0, 50));
+			m_system->constrainPosition(m_patch.massIndex(50, 0));
+			constrainedMasses.push_back(m_patch.massIndex(50, 0));
+			m_system->constrainPosition(m_patch.massIndex(50, 50));
+			constrainedMasses.push_back(m_patch.massIndex(50, 50));
+
+			//Affichage
+			HelperGl::Material patchMat;
+			patchMat.setDiffuse(HelperGl::Color(0.0f, 1.0f, 0.0f));
+			m_patchGraph = new SceneGraph::Patch(51, 51, patchMat);
+			m_root.addSon(m_patchGraph);
+
+			//******//
+
+			//Sol
+			m_systemGround = new Animation::SpringMassSystem();
+			{
+				Math::Vector4f row1 = Math::makeVector(1.0f, 0.0f, 0.0f, -10.0f / 2.0f);
+				Math::Vector4f row2 = Math::makeVector(0.0f, 1.0f, 0.0f, -10.0f / 2.0f);
+				Math::Vector4f row3 = Math::makeVector(0.0f, 0.0f, 1.0f, -1.5f);
+				Math::Vector4f row4 = Math::makeVector(0.0f, 0.0f, 0.0f, 1.0f);
+				Math::Matrix4x4f g_transform(row1, row2, row3, row4);
+				m_systemGround->createPatch(10, 2, 10, 2, 1, 0, g_transform);
+			}
+			//Affichage Sol
+			HelperGl::Material groundMat;
+			groundMat.setDiffuse(HelperGl::Color(0.2, 0.2, 0.2));
+			m_groundGraph = new SceneGraph::Patch(3, 3, groundMat);
+			m_root.addSon(m_groundGraph);
+		
+			//Contrainte sol
+			m_system->addPositionConstraint(computeGround);
+
+			//******//
+
+			//Sphere
+			HelperGl::Material sphereMat;
+			sphereMat.setDiffuse(HelperGl::Color(1.0f, 0.0f, 0.0f));
+			SceneGraph::Sphere * sphere = new SceneGraph::Sphere(sphereMat, 0.2f);
+			SceneGraph::Translate * sphereTranslate = new SceneGraph::Translate(Math::makeVector(0.0f, -0.3f, -1.0f));
+			sphereTranslate->addSon(sphere);
+
+			m_root.addSon(sphereTranslate);
+			
+			//Contrainte sol
+			m_system->addPositionConstraint(computeSphere);
 		}
 
 		virtual void initializeRendering()
@@ -92,6 +221,10 @@ namespace Application
 		{
 			handleKeys();
 			GL::loadMatrix(m_camera.getInverseTransform());
+
+			m_system->copyMassesPositionsTo(m_patchGraph->getVertices().begin());
+			m_systemGround->copyMassesPositionsTo(m_groundGraph->getVertices().begin());
+			m_system->update(dt);
 			m_root.draw();
 
 		}
